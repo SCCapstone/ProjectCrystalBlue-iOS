6f18cd5d570ef0ba9a1a78f4ee06561016225457
hoodlm <hoodlma@gmail.com>
2013-10-26 12:26:25 -0400

    Blank iOS XCODE project

---
+++ ProjectCrystalBlue-iOS/ProjectCrystalBlue-iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProjectCrystalBlue-iOS
+//
+//  Created by Logan Hood on 10/26/13.
+//  Copyright (c) 2013 Project Crystal Blue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
