8063aea1028ee23470a95fd64171941f7d10b0ca
Ryan McGraw <r.mcgraw2010@gmail.com>
2014-02-23 23:39:57 -0500

    added all of the procedures except amp magnet procedures

--- ProjectCrystalBlue-iOS/ProjectCrystalBlue-iOS/Procedures.h
+++ ProjectCrystalBlue-iOS/ProjectCrystalBlue-iOS/Procedures.h
@@ -20,6 +20,27 @@
 @interface Procedures : NSObject
 
 /**
+ * Make a slab from a sample. This creates a new sample and adds slab to its procedures
+ **/
++ (void)makeSlabfromSample:(Sample *)sample
+              withInitials:(NSString *)initials
+                   inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ * Make a billet from a sample. This creates a new sample and adds billet to its procedures
+ **/
++ (void)makeBilletfromSample:(Sample *)sample
+                withInitials:(NSString *)initials
+                     inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ * Make a thin section from a sample. This creates a new sample and adds thin section to its procedures
+ **/
++ (void)makeThinSectionfromSample:(Sample *)sample
+                     withInitials:(NSString *)initials
+                          inStore:(AbstractLibraryObjectStore *)store;
+
+/**
  *  Jawcrush the given sample. This is an IN-PLACE operation, destroying the previous sample.
  */
 + (void)jawCrushSample:(Sample *)sample
@@ -34,11 +55,140 @@
                 inStore:(AbstractLibraryObjectStore *)store;
 
 /**
- *  Sample was consider pan up. This creates a new sample and adds this attribute
+ *  Trim the given sample. This is an IN-PLACE operation, destroying the previous sample.
+ */
++ (void)trimSample:(Sample *)sample
+      withInitials:(NSString *)initials
+           inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered gemeni up. This creates a new sample and adds this attribute and appends down
+ *  to the current sample.
+ */
++ (void)gemeniUpSample:(Sample *)sample
+          withInitials:(NSString *)initials
+               inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered gemeni up. This creates a new sample and adds this attribute and appends down
+ *  to the current sample.
+ */
++ (void)geminiDownSample:(Sample *)sample
+            withInitials:(NSString *)initials
+                 inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered pan up. This creates a new sample and adds this attribute and appends down 
+ *  to the current sample.
  */
 + (void)panUpSample:(Sample *)sample
 withInitials:(NSString *)initials
             inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered pan down. This creates a new sample and adds this attribute and appends up
+ *  to the current sample.
+ */
++ (void)panDownSample:(Sample *)sample
+       withInitials:(NSString *)initials
+            inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered sieves up. This creates a new sample and adds this attribute and appends down
+ *  to the current sample.
+ */
++ (void)sievesTenUpSample:(Sample *)sample
+             withInitials:(NSString *)initials
+                  inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered sieves down. This creates a new sample and adds this attribute and appends up
+ *  to the current sample.
+ */
++ (void)sievesTenDownSample:(Sample *)sample
+               withInitials:(NSString *)initials
+                    inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered hL330 up. This creates a new sample and adds this attribute and appends down
+ *  to the current sample.
+ */
++ (void)heavyLiquid_330_UpSample:(Sample *)sample
+                    withInitials:(NSString *)initials
+                         inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered hL330 down. This creates a new sample and adds this attribute and appends up
+ *  to the current sample.
+ */
++ (void)heavyLiquid_330_DownSample:(Sample *)sample
+                      withInitials:(NSString *)initials
+                           inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered hL290 up. This creates a new sample and adds this attribute and appends down
+ *  to the current sample.
+ */
++ (void)heavyLiquid_290_UpSample:(Sample *)sample
+                    withInitials:(NSString *)initials
+                         inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered hL290 down. This creates a new sample and adds this attribute and appends up
+ *  to the current sample.
+ */
++ (void)heavyLiquid_290_DownSample:(Sample *)sample
+                      withInitials:(NSString *)initials
+                           inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered hL265 up. This creates a new sample and adds this attribute and appends down
+ *  to the current sample.
+ */
++ (void)heavyLiquid_265_UpSample:(Sample *)sample
+                    withInitials:(NSString *)initials
+                         inStore:(AbstractLibraryObjectStore *)store;
+
+
+/**
+ *  Sample was considered hL265 down. This creates a new sample and adds this attribute and appends up
+ *  to the current sample.
+ */
++ (void)heavyLiquid_265_DownSample:(Sample *)sample
+                      withInitials:(NSString *)initials
+                           inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered hL255 up. This creates a new sample and adds this attribute and appends down
+ *  to the current sample.
+ */
++ (void)heavyLiquid_255_UpSample:(Sample *)sample
+                    withInitials:(NSString *)initials
+                         inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered hL255 down. This creates a new sample and adds this attribute and appends up
+ *  to the current sample.
+ */
++ (void)heavyLiquid_255_DownSample:(Sample *)sample
+                      withInitials:(NSString *)initials
+                           inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered handMagnet up.This creates a new sample and adds this attribute and appends down
+ *  to the current sample.
+ */
++ (void)handMagnetUpSample:(Sample *)sample
+              withInitials:(NSString *)initials
+                   inStore:(AbstractLibraryObjectStore *)store;
+
+/**
+ *  Sample was considered handMagnet up.This creates a new sample and adds this attribute and appends down
+ *  to the current sample.
+ */
++ (void)handMagnetDownSample:(Sample *)sample
+                withInitials:(NSString *)initials
+                     inStore:(AbstractLibraryObjectStore *)store;
 
 
 /**
